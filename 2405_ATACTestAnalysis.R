fdc873450c8c6c4661fa808c4b5713e740ca2c51
LineWulff <lw@himalaya.dk>
2024-05-16 13:49:17 -0600

    update

--- 2405_ATACTestAnalysis.R
+++ 2405_ATACTestAnalysis.R
@@ -1,6 +1,7 @@
-#' R script for producing outputs in from Fabien's rewilded vs. SPF mice
+#' R script for testing scATAC analysis w. Seurat and Signac
 #' Author: Line Wulff
-#' Date (created): 23-08-18
+#' Date (created): 24-05-16
+#' # Based on https://stuartlab.org/signac/articles/pbmc_vignette
 
 #' #### ---- Initiate libraries ---- ####
 library(ggplot2)
@@ -9,7 +10,10 @@ library(ggrastr)
 library(viridis)
 library(scales)
 library(Signac)
+library(Seurat)
 
 #### ---- variables used throughout script ---- ####
 projdir <- getwd()
 dato <- str_sub(str_replace_all(Sys.Date(),"-","_"), 3, -1)
+
+
